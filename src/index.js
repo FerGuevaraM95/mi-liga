98600b96093a9d542c64508d5d2caa82d2a13564
Fernando Guevara <luisfernandoguevara95@gmail.com>
2018-07-14 14:11:12 -0500

    Add components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import { render } from "react-dom";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Material
+// import Reboot from "material-ui/Reboot";
+
+// Estilos
+import "./index.css";
+
+// Componentes
+import Header from "./components/common/Header/Header";
+import Liga from "./components/Liga/Liga";
+import Calendario from "./components/Calendario/Calendario"
+
+// The CODE!
+const Root = () => {
+  return (
+    <div>
+      {/* <Reboot /> */}
+      <Header />
+      <div className="contenedor">
+          <Liga />
+          <Calendario />
+      </div>
+    </div>
+  );
+};
+
+render(<Root />, document.querySelector("#root"));
+
 registerServiceWorker();
