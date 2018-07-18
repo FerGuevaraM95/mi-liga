1512da80b1002646b6604d2ff21ec509b041c5ab
Fernando Guevara <luisfernandoguevara95@gmail.com>
2018-07-18 17:33:14 -0500

    Add favorite star

--- src/components/common/Equipo/Equipo.js
+++ src/components/common/Equipo/Equipo.js
@@ -3,11 +3,48 @@ import React, { Component } from 'react';
 // Estilos
 import './Equipo.css';
 
+// Material
+import Icon from 'material-ui/Icon';
+
 class Equipo extends Component {
+
+    state = {
+        fav: {value: 'star'}
+    }
+
+    setFav() {
+        console.log("Favorito!");
+
+        // Respaldamos el estado existente
+        const fav = {...this.state.fav};
+
+        // Modificamos el valor
+        fav.value = fav.value === 'star' ? 'star_selected' : 'star';
+
+        // Recargamos el estado
+        this.setState({ fav });
+    }
+
+    componentDidMount() {
+        const localFavs = localStorage.getItem(this.props.nombre);
+        if(localFavs) {
+            this.setState({
+                fav: JSON.parse(localFavs)
+            })
+        }
+    }
+
+    componentDidUpdate() {
+        console.log(this.props);
+        console.log(this.state.fav);
+        localStorage.setItem(this.props.nombre, JSON.stringify(this.state.fav))
+    }
+
     render() {
         return (
             <div>
                 <img className="team-logo" src={this.props.logo} alt={this.props.nombre} />
+                <Icon onClick={this.setFav.bind(this)} className={this.state.fav.value}>start_rate</Icon>
             </div>
         );
     }
